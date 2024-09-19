644ffd5e1f895abd1dbfca8a44fb0aa5e2093ad4
RashmeenBhullar <122423161+RashmeenBhullar@users.noreply.github.com>
2024-09-19 11:04:25 +0530

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,17 @@
+
+// class Solution {
+//     // Function to reverse words in a given string.
+//     String reverseWords(String str) {
+//         String[] word=str.split("\\.");
+//        StringBuilder r=new StringBuilder();
+//      for(int i=word.length-1;i>=0;i--){
+//          r.append(word[i]);
+//          if(i!=0){
+//              r.append(".");
+//          }
+         
+//      }
+       
+//        return r.toString();
+//     }
+// }
